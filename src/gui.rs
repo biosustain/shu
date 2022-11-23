f1c3a4928d9bc102f1c18c1ad583036631aca7d3
carrascomj <carrascomurielj@gmail.com>
2022-11-23 17:53:41 +0100

    refactor: remove unused UI panel

--- src/gui.rs
+++ src/gui.rs
@@ -8,94 +8,15 @@ pub struct GuiPlugin;
 
 impl Plugin for GuiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(ui_example).add_system(file_drop);
+        app.add_system(file_drop);
     }
 }
-
-fn ui_example(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // root node
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                justify_content: JustifyContent::SpaceBetween,
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|parent| {
-            // left vertical fill (border)
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Px(170.0), Val::Percent(30.0)),
-                        border: UiRect::all(Val::Px(2.0)),
-                        ..default()
-                    },
-                    background_color: Color::rgb(0.15, 0.15, 0.15).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                                flex_wrap: FlexWrap::Wrap,
-                                flex_direction: FlexDirection::Column,
-                                ..default()
-                            },
-                            background_color: Color::rgb(0.95, 0.95, 0.95).into(),
-                            ..default()
-                        })
-                        .insert(MyDropTarget)
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn(
-                                TextBundle::from_section(
-                                    "Drop data!",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 30.0,
-                                        color: Color::rgb(0.15, 0.15, 0.15).into(),
-                                    },
-                                )
-                                .with_style(Style {
-                                    margin: UiRect::all(Val::Px(5.0)),
-                                    ..default()
-                                }),
-                            );
-                        })
-                        .with_children(|parent| {
-                            parent.spawn(
-                                TextBundle::from_section(
-                                    "Reaction data should end\nwith '.reaction.json",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 15.0,
-                                        color: Color::rgb(0.15, 0.15, 0.15).into(),
-                                    },
-                                )
-                                .with_style(Style {
-                                    margin: UiRect::all(Val::Px(5.0)),
-                                    max_size: Size::new(Val::Percent(50.0), Val::Percent(100.0)),
-                                    flex_wrap: FlexWrap::Wrap,
-                                    ..default()
-                                }),
-                            );
-                        });
-                });
-        });
-}
-#[derive(Component)]
-struct MyDropTarget;
 
 fn file_drop(
     mut dnd_evr: EventReader<FileDragAndDrop>,
     asset_server: Res<AssetServer>,
     mut reaction_resource: ResMut<ReactionState>,
     mut escher_resource: ResMut<MapState>,
-    query_ui_droptarget: Query<&Interaction, With<MyDropTarget>>,
 ) {
     for ev in dnd_evr.iter() {
         if let FileDragAndDrop::DroppedFile { id, path_buf } = ev {
