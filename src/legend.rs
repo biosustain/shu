7b3df614d78e0ecb3ffd1863fea70035528a4f9e
carrascomj <carrascomurielj@gmail.com>
2023-01-27 17:16:51 +0100

    feat: display metabolite legend

--- src/legend.rs
+++ src/legend.rs
@@ -5,7 +5,7 @@ use bevy::{prelude::*, utils::HashMap};
 use crate::{
     aesthetics::{Aesthetics, Gcolor, Point},
     funcplot::{linspace, max_f32, min_f32, ScaleBundle},
-    geom::{Drag, GeomArrow, HistPlot, Side, Xaxis},
+    geom::{Drag, GeomArrow, HistPlot, Side, Xaxis, GeomMetabolite},
     gui::UiState,
 };
 
@@ -15,12 +15,15 @@ impl Plugin for LegendPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_legend)
             // .add_system(draw_legend_for_axis)
-            .add_system(color_legend);
+            .add_system(color_legend_arrow)
+            .add_system(color_legend_circle);
     }
 }
 
 #[derive(Component)]
-struct Legend;
+struct LegendArrow;
+#[derive(Component)]
+struct LegendCircle;
 #[derive(Component)]
 struct Xmin;
 #[derive(Component)]
@@ -30,19 +33,23 @@ fn spawn_legend(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    let scales = ScaleBundle::new(0., 0., 0., 200., 200., font, 20.);
-    let img_handle = asset_server.load("arrow_grad.png");
+    let font = asset_server.load("fonts/Assistant-Regular.ttf");
+    let scales_arrow = ScaleBundle::new(0., 0., 0., 200., 200., font.clone(), 20., Color::hex("504d50").unwrap());
+    let scales_mets = ScaleBundle::new(0., 0., 0., 200., 200., font, 20., Color::hex("504d50").unwrap());
+    let arrow_handle = asset_server.load("arrow_grad.png");
+    let met_handle = asset_server.load("met_grad.png");
     commands
         .spawn(NodeBundle {
             style: Style {
-                size: Size::new(Val::Px(400.), Val::Px(50.)),
+                size: Size::new(Val::Px(400.), Val::Px(120.)),
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems::Center,
                 padding: UiRect::left(Val::Px(10.0)),
                 ..Default::default()
             },
             ..Default::default()
         })
-        // legend arrow
+        // arrow arrow
         .with_children(|p| {
             p.spawn(NodeBundle {
                 style: Style {
@@ -53,11 +60,11 @@ fn spawn_legend(
                 },
                 ..Default::default()
             })
-            .insert(Legend)
+            .insert(LegendArrow)
             .with_children(|p| {
                 p.spawn((
                     TextBundle {
-                        text: scales.x_0.text,
+                        text: scales_arrow.x_0.text,
                         ..default()
                     },
                     Xmin,
@@ -69,14 +76,55 @@ fn spawn_legend(
                         size: Size::new(Val::Px(200.0), Val::Px(50.0)),
                         ..default()
                     },
-                    image: UiImage(img_handle),
+                    image: UiImage(arrow_handle),
                     ..default()
                 });
             })
             .with_children(|p| {
                 p.spawn((
                     TextBundle {
-                        text: scales.x_n.text,
+                        text: scales_arrow.x_n.text,
+                        ..default()
+                    },
+                    Xmax,
+                ));
+            });
+        })
+        // metabolite legend
+        .with_children(|p| {
+            p.spawn(NodeBundle {
+                style: Style {
+                    size: Size::new(Val::Px(160.), Val::Px(50.)),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::SpaceBetween,
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(LegendCircle)
+            .with_children(|p| {
+                p.spawn((
+                    TextBundle {
+                        text: scales_mets.x_0.text,
+                        ..default()
+                    },
+                    Xmin,
+                ));
+            })
+            .with_children(|p| {
+                p.spawn(ImageBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(50.0), Val::Px(50.0)),
+                        ..default()
+                    },
+                    image: UiImage(met_handle),
+                    ..default()
+                });
+            })
+            .with_children(|p| {
+                p.spawn((
+                    TextBundle {
+                        text: scales_mets.x_n.text,
                         ..default()
                     },
                     Xmax,
@@ -86,9 +134,9 @@ fn spawn_legend(
         .insert(Drag::default());
 }
 
-fn color_legend(
+fn color_legend_arrow(
     ui_state: Res<UiState>,
-    mut legend_query: Query<(Entity, &Children), With<Legend>>,
+    mut legend_query: Query<(Entity, &Children), With<LegendArrow>>,
     mut img_query: Query<&UiImage>,
     mut text_query: Query<&mut Text, With<Xmin>>,
     mut text_max_query: Query<&mut Text, Without<Xmin>>,
@@ -120,10 +168,66 @@ fn color_legend(
                 } else if let Ok(img_legend) = img_query.get_mut(*child) {
                     let handle = images.get_handle(&img_legend.0);
                     let image = images.get_mut(&handle).unwrap();
-                    let points = linspace(min_val, max_val, 400);
+
+                    let width = image.size().x as f64;
+                    let points = linspace(min_val, max_val, width as u32);
                     let data = image.data.chunks(4).enumerate().flat_map(|(i, pixel)| {
-                        let row = (i as f64 / 400.).floor() as f64;
-                        let x = i as f64 - 400. * row;
+                        let row = (i as f64 / width).floor();
+                        let x = i as f64 - width * row;
+                        if pixel[3] != 0 {
+                            let color = grad.at(points[x as usize] as f64).to_rgba8();
+                            [color[0], color[1], color[2], color[3]].into_iter()
+                        } else {
+                            [0, 0, 0, 0].into_iter()
+                        }
+                    });
+                    image.data = data.collect::<Vec<u8>>();
+                }
+            }
+        }
+    }
+}
+
+fn color_legend_circle(
+    ui_state: Res<UiState>,
+    mut legend_query: Query<(Entity, &Children), With<LegendCircle>>,
+    mut img_query: Query<&UiImage>,
+    mut text_query: Query<&mut Text, With<Xmin>>,
+    mut text_max_query: Query<&mut Text, Without<Xmin>>,
+    point_query: Query<(&Point<f32>, &Aesthetics), (With<Gcolor>, With<GeomMetabolite>)>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    for (colors, aes) in point_query.iter() {
+        if let Some(condition) = &aes.condition {
+            if condition != &ui_state.condition {
+                continue;
+            }
+        }
+        for (_parent, children) in &mut legend_query {
+            let min_val = min_f32(&colors.0);
+            let max_val = max_f32(&colors.0);
+            let grad = crate::funcplot::build_grad(
+                ui_state.zero_white,
+                min_val,
+                max_val,
+                &ui_state.min_metabolite_color,
+                &ui_state.max_metabolite_color,
+            );
+            // modify the image inplace
+            for child in children.iter() {
+                if let Ok(mut text) = text_query.get_mut(*child) {
+                    text.sections[0].value = format!("{:.2e}", min_val);
+                } else if let Ok(mut text) = text_max_query.get_mut(*child) {
+                    text.sections[0].value = format!("{:.2e}", max_val);
+                } else if let Ok(img_legend) = img_query.get_mut(*child) {
+                    let handle = images.get_handle(&img_legend.0);
+                    let image = images.get_mut(&handle).unwrap();
+
+                    let width = image.size().x as f64;
+                    let points = linspace(min_val, max_val, width as u32);
+                    let data = image.data.chunks(4).enumerate().flat_map(|(i, pixel)| {
+                        let row = (i as f64 / width).floor();
+                        let x = i as f64 - width * row;
                         if pixel[3] != 0 {
                             let color = grad.at(points[x as usize] as f64).to_rgba8();
                             [color[0], color[1], color[2], color[3]].into_iter()
